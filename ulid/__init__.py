539b93e494d58f935efb702650d0062cf02ed5fb
Andrew Hawker <andrew.r.hawker@gmail.com>
2017-06-10 19:08:42 -0700

    Add ulid package.

---
+++ ulid/__init__.py
@@ -0,0 +1,12 @@
+"""
+    ulid
+    ~~~~
+
+    Universally Unique Lexicographically Sortable Identifier
+
+    :copyright: (c) 2017 Andrew Hawker.
+    :license: Apache 2.0, see LICENSE for more details.
+"""
+
+__all__ = []
+__version__ = '0.0.1'
