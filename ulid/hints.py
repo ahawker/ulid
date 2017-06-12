5f9efed606eca399ba198e5218eb17bbe864e390
Andrew Hawker <andrew.r.hawker@gmail.com>
2017-06-11 18:23:03 -0700

    Add hints module for reusable type hint definitons.

---
+++ ulid/hints.py
@@ -0,0 +1,22 @@
+"""
+    ulid/hints
+    ~~~~~~~~~~
+
+    Contains type hint definitions for the package.
+"""
+import typing
+
+
+#: Type hint that defines multiple types that implement the buffer protocol
+#: that can encoded into a Base32 string.
+Buffer = typing.Union[bytes, bytearray, memoryview]
+
+
+#: Type hint that defines multiple primitive types that can represent
+#: a Unix timestamp in seconds.
+TimestampPrimitive = typing.Union[int, float, str, bytes, bytearray, memoryview]
+
+
+#: Type hint that defines multiple primitive types that can represent
+#: randomness.
+RandomnessPrimitive = typing.Union[int, float, str, bytes, bytearray, memoryview]
