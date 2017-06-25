3a653bc2f4c53c985c14c6142a667c0e9078d52a
Andrew Hawker <andrew.r.hawker@gmail.com>
2017-06-25 12:29:45 -0700

    Bump version: 0.0.2 → 0.0.3

--- docs/conf.py
+++ docs/conf.py
@@ -49,7 +49,7 @@ author = 'Andrew Hawker'
 # built documents.
 #
 # The short X.Y version.
-version = '0.0.2'
+version = '0.0.3'
 # The full version, including alpha/beta/rc tags.
 release = version
 
