e0d184397b0205dd45b51ee4d587ad2f8f697a6e
Andrew Hawker <andrew.r.hawker@gmail.com>
2017-10-28 15:35:12 -0700

    Add test for Github issue 61.

--- tests/test_bugs.py
+++ tests/test_bugs.py
@@ -4,6 +4,8 @@
 
     Tests for validating reported bugs have been fixed.
 """
+import pytest
+
 from ulid import api
 
 
@@ -19,3 +21,17 @@ def test_github_issue_58():
     value = '01BX73KC0TNH409RTFD1JXKmO0'
     instance = api.from_str(value)
     assert instance.str == '01BX73KC0TNH409RTFD1JXKM00'
+
+
+def test_github_issue_61():
+    """
+    Assert that :func:`~ulid.api.from_str` will raise a :class:`~ValueError` when given a string
+    that contains values not in the Base32 character set.
+
+    In this example, the main character to test is "u" and "U".
+
+    Issue: https://github.com/ahawker/ulid/issues/61
+    """
+    for s in ('01BX73KC0TNH409RTFD1uXKM00', '01BX73KC0TNH409RTFD1UXKM00'):
+        with pytest.raises(ValueError):
+            api.from_str(s)
