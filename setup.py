29b8e7a426ef983587e9ef92791d944b4375fc12
Andrew Hawker <andrew.r.hawker@gmail.com>
2018-04-04 21:05:27 -0700

    Drop py33 support.

--- setup.py
+++ setup.py
@@ -47,7 +47,6 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
