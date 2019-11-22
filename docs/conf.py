2c148ca04cf080c1b224c4b4e0ca447581f8b78a
Andrew Hawker <andrew.r.hawker@gmail.com>
2019-11-22 12:30:25 -0800

    Bump version: 0.0.11 → 0.0.12

--- docs/conf.py
+++ docs/conf.py
@@ -49,7 +49,7 @@ author = 'Andrew Hawker'
 # built documents.
 #
 # The short X.Y version.
-version = '0.0.11'
+version = '0.0.12'
 # The full version, including alpha/beta/rc tags.
 release = version
 
