64db8e687fcb5faaf68c92dc5d8adef2b4b1bddd
Andrew Hawker <andrew.r.hawker@gmail.com>
2020-06-17 11:16:36 -0700

    Explicitly define public api in __init__.py
    This is required because mypy --strict uses --no-implicit-reexport.
    See: https://github.com/python/mypy/issues/7423

--- ulid/__init__.py
+++ ulid/__init__.py
@@ -8,8 +8,19 @@
     :license: Apache 2.0, see LICENSE for more details.
 """
 from . import api, ulid
-from .api import *  # pylint: disable=wildcard-import
-from .ulid import *  # pylint: disable=wildcard-import
+
+from_bytes = api.from_bytes
+from_int = api.from_int
+from_randomness = api.from_randomness
+from_str = api.from_str
+from_timestamp = api.from_timestamp
+from_uuid = api.from_uuid
+new = api.new
+parse = api.parse
+
+Timestamp = ulid.Timestamp
+Randomness = ulid.Randomness
+ULID = ulid.ULID
 
 __all__ = api.__all__ + ulid.__all__
 
